7abbbbe21dbba221577d116f03a396754d0ad6dc
Joseph Tu <joetu85@gmail.com>
2014-12-03 10:39:26 -0600

    Added Category Model and migrations

--- Ecomm_Project/app/models/category.rb
+++ Ecomm_Project/app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  has_many :products
+  validates :category_name, presence: true
 end
--- Ecomm_Project/app/models/product.rb
+++ Ecomm_Project/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  belongs_to :category
   validates :product_name, presence:true
   validates :price, presence: true
   validates :quantity, numericality: true
